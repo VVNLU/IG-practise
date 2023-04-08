2db6450580257b35beb612c57e733c5db66533c2
vivian <vivianlu512@gmail.com>
2023-04-08 22:29:16 +0800

    add router

---
+++ frontend/src/routes.js
@@ -0,0 +1,41 @@
+import HomePage from './pages/HomePage.vue';
+import LoginPage from './pages/LoginPage.vue';
+import ProfileEdittingPage from './pages/ProfileEdittingPage.vue';
+import ProfilePage from './pages/ProfilePage.vue';
+import SearchPage from './pages/SearchPage.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomePage,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginPage,
+  },
+  {
+    path: '/profile/edit',
+    name: 'editprofile',
+    component: ProfileEdittingPage,
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: ProfilePage,
+  },
+  {
+    path: '/search_result',
+    name: 'search_result',
+    component: SearchPage,
+  },
+];
+
+const router = createRouter({
+  routes: routes,
+  history: createWebHistory(),
+});
+
+export { router };
